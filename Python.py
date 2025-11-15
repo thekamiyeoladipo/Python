17e868f6fb2373479a363bb1dfc80d7907df6a39
kamiye <kamiye31@gmail.com>
2025-11-15 19:37:01 +0100

    Initial commit of Python project

---
+++ Python.py
@@ -0,0 +1,15 @@
+print("Kamiye is learning Python.")
+
+# The most common data types in Python are:
+# 1. Integer example
+x = 5   
+print("Integer:", x)
+# 2. Float example
+y = 3.14
+print("Float:", y)
+# 3. String example 
+name = "Kamiye"
+print("String:", name)
+# 4. Boolean example
+is_learning = True
+print("Boolean:", is_learning)
