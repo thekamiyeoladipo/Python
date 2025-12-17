3dcef63c5bc4e755597659cb7a9a5b374b9452f7
kamiye <kamiye31@gmail.com>
2025-12-17 15:09:06 +0100

    Add character creation function with validation for name and stats

--- Python.py
+++ Python.py
@@ -126,3 +126,27 @@ def caesar(text, shift):
         print(text)
     else:
         encrypted_text = text.translate(translation_table)
+
+
+
+full_dot = '●'
+empty_dot = '○'
+def create_character(name, strength, intelligence, charisma):
+    if not isinstance(name, str):
+        return "The character name should be a string"
+    if name == "":
+        return "The character should have a name"
+    if len(name) > 10:
+        return "The character name is too long"
+    if name == " ":
+        return "The character name should not contain spaces"
+    stats = [strength, intelligence, charisma]
+    if not all(isinstance(stat, int) for stat in stats):
+        return "All stats should be integers"
+    if not all(stat >= 1 for stat in stats):
+        return "All stats should be no less than 1."
+    if not all(stat <= 4 for stat in stats):
+        return "All stats should be no more than 4."
+    if sum(stats) != 7:
+        return "The character should start with 7 points."
+        
